1bb180270894a40c37df3e8e4e3b93df6979b40b
Sofiia Budilova <avolidub@gmail.com>
2024-11-07 03:10:40 +0200

    :sparkles: Add basic grammar rules

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use pest::Parser;
 use anyhow::anyhow;
 use natural_date_parser::*;
+use pest::Parser;
 
-fn main() -> anyhow::Result< () > {
-    let pair = Grammar::parse(Rule::field, "-273.15")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+fn main() -> anyhow::Result<()> {
+    let pair = Grammar::parse(Rule::date_expression, "next Monday")?
+        .next()
+        .ok_or_else(|| anyhow!("no pair"))?;
     dbg!(pair);
     Ok(())
 }
